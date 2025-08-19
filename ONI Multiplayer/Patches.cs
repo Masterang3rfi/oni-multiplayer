b9ffade81ae80274dbd2182f23edb884d8efa3f2
Masterang3rfi <masterang3rfi@gmail.com>
2025-08-19 00:17:53 -0400

    update

--- ONI Multiplayer/Patches.cs
+++ ONI Multiplayer/Patches.cs
@@ -1,23 +1,25 @@
 using HarmonyLib;
+using Klei;
 
 namespace ONI_Multiplayer
 {
-    public class Patches
+    public class MainMenu_OnPrefabInit_Patch
     {
-        [HarmonyPatch(typeof(Db))]
-        [HarmonyPatch("Initialize")]
+        [HarmonyPatch(typeof(MainMenu))]
+        [HarmonyPatch("OnPrefabInit")]
         public class Db_Initialize_Patch
         {
             public static void Prefix()
             {
-                Debug.Log("I execute before Db.Initialize!");
+                Debug.Log("I execute before MainMenu.OnPrefabInit!");
             }
 
             public static void Postfix()
             {
-                Debug.Log("I execute after Db.Initialize!");
+                
+               
+
             }
         }
     }
 }
-#test
